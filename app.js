126a500692b17cb327682351cf6f55fc8c5c265e
David Lerner <lernerlombrozo@gmail.com>
2019-11-20 17:44:51 -0700

    good

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 let x0 = 0; // initial x
-let xf = 1; // final x
+let xf = 10; // final x
 let n = 4; // for composite plots
 let h = ((xf-x0)/n); // this h is the distance between two points.
 let degree1=2;
-let degree2=3;
-const smallH = 0.001; // this h is for making the plot continuous.
+let degree2=1;
+const smallH = 0.01; // this h is to make plot continuous.
 const xT="x";
 const yT= "f(x)";
 
 function toSolve(x){
-  return (Math.sqrt(1-x**2)) // Circle centered in (0,0) with radius 1. Can be evaluated from -1 to 1;
-  // return (Math.exp(x))
+  // return (Math.sqrt(1-x**2)) // Circle centered in (0,0) with radius 1. Can be evaluated from -1 to 1;
+  // return (Math.exp(x));
+  // return x**3 + 3*x**2;
+  return Math.sin(x)
 }
-
-var alert = new Vue({
-    el: '#alert',
-    data: {
-      message: 'Hello Vue!',
-      seen: false
-    }
-})
 
 var app = new Vue({
     el: '#app',
@@ -30,26 +24,31 @@ var app = new Vue({
       degree1:degree1,
       degree2:degree2,
       calculating:false,
-      showMessage:false,
-      message: "n must be multiple of the biggest polynomial",
+      message: "",
     },
     methods: {
         makeCharts: function () {
             this.calculating = true;
             x0 = +this.x0;
             xf = +this.xf;
-            n = +this.n;
+            this.n=Math.round(+this.n)
+            n = this.n;
             h = ((xf-x0)/n);
             degree1=+this.degree1;
             degree2=+this.degree2;
             this.calculating = false;
-            this.showMessage = false;
+            this.message = "";
+            if(n <1){
+              this.message="n must be a positive integer";
+              return
+            }
             if(n % degree1 == 0 && n % degree2 == 0){
               makeCompositePlot();
+              makeCompositePlot2();
               makeSimplePlot();
             } else {
-              console.log(n % degree1, n % degree2)
-              this.showMessage = true;
+                console.log(n)
+                this.message = "n must be multiple of the all polynomials";
             }
         }
       }
--- plots/composite.js
+++ plots/composite.js
@@ -1,9 +1,10 @@
-function makeCompositeTable(x0,xf,h,smallH,xTitle,yTitle,tTitle,sTitle){
-    var table =[[xTitle, yTitle, tTitle, sTitle]];
+function makeCompositeTable(x0,xf,h,degree,smallH,xTitle,yTitle,pTitle){
+    console.log(x0,xf,h,degree,smallH,xTitle,yTitle,pTitle);
+    var table =[[xTitle, yTitle, pTitle]];
     for(let i = x0;i<xf;i=i+h){
         let xb = i+h;
         for(let j = i;j<xb;j=j+smallH){
-            table.push([j,toSolve(j.toPrecision(7)),lagrangeFromDegree(j,i,xb,degree1),lagrangeFromDegree(j,i,xb,degree2)])
+            table.push([j,toSolve(j.toPrecision(7)),lagrangeFromDegree(j,i,xb,degree)])
         }
     }
     return table;
@@ -15,23 +16,49 @@ function makeCompositePlot(){
     google.charts.setOnLoadCallback(drawChart);
 
     // console.log(n)
-    var table = makeCompositeTable(x0,xf,h,smallH,xT,yT,`n=${degree1}`,`n=${degree2}`)
+    var table = makeCompositeTable(x0,xf,h,degree1, smallH,xT,yT,`n=${degree1}`)
+    // console.log(table);
+
+    function drawChart() {
+    var data = google.visualization.arrayToDataTable(table);
+
+    var options = {
+        title: `n=${degree1}`,
+        hAxis: {title: xT,  titleTextStyle: {color: '#333'}},
+        vAxis: {title: yT, minValue: 0}
+    };
+
+    if(!composite){
+        composite = new google.visualization.AreaChart(document.getElementById('composite_chart_div'));
+    }
+    composite.draw(data, options);
+    }
+}
+
+let composite2;
+function makeCompositePlot2(){
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    // console.log(n)
+    var table = makeCompositeTable(x0,xf,h,degree2,smallH,xT,yT,`n=${degree2}`)
     console.log(table);
 
     function drawChart() {
     var data = google.visualization.arrayToDataTable(table);
 
     var options = {
-        title: `n=${degree1} and n=${degree2}`,
+        title: `n=${degree2}`,
         hAxis: {title: xT,  titleTextStyle: {color: '#333'}},
         vAxis: {title: yT, minValue: 0}
     };
 
-    if(!composite){
-        var composite = new google.visualization.AreaChart(document.getElementById('composite_chart_div'));
+    if(!composite2){
+        composite2 = new google.visualization.AreaChart(document.getElementById('composite_2_chart_div'));
     }
-    composite.draw(data, options);
+    composite2.draw(data, options);
     }
 }
 
-makeCompositePlot()
+makeCompositePlot();
+makeCompositePlot2();
