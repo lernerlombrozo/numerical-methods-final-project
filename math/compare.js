a0a6ffb238bfe39afb7feeeae711721a4deafeaf
David Lerner <lernerlombrozo@gmail.com>
2019-11-23 11:57:04 -0700

    fixed comparisson

--- math/compare.js
+++ math/compare.js
@@ -1,18 +1,18 @@
-function makeComparissonChart(x0,xf,h){
+function makeComparissonChart(x0,xf,n){
     let ref;
     if(analyticalSolution) ref = analyticalSolution
-    else ref= compositeSimpson(x0,xf,0.00001);
+    else ref= compositeSimpson(x0,xf,100000);
     const trap = trapezoid(x0,xf);
     const simp = simpson(x0,xf);
     const simp38 = simpson38(x0,xf);
-    const cTrap = compositeTrapezoid(x0,xf,h)
-    const cSimp = compositeSimpson(x0,xf,h)
-    const cSimp38 = compositeSimpson38(x0,xf,h)
+    const cTrap = compositeTrapezoid(x0,xf,n)
+    const cSimp = compositeSimpson(x0,xf,n)
+    const cSimp38 = compositeSimpson38(x0,xf,n)
     const GQ2 = gaussianQuadrature(x0,xf,2)
     const GQ3 = gaussianQuadrature(x0,xf,3)
     const table = [
         ['Method',`integral from ${x0} to ${xf}`,'error'],
-        [`Reference ${analyticalSolution? 'analytical solution': '(composite n=2 h=0.00001)' }`, ref,null],
+        [`Reference: ${analyticalSolution? 'analytical solution': '(composite n=2 divisions=1000000)' }`, ref,null],
         ["Newton-Cotes n=1",trap ,Math.abs(ref-trap)],
         ["Newton-Cotes n=2", simp, Math.abs(ref-simp)],
         ["Newton-Cotes n=3", simp38,Math.abs(ref-simp38)],
