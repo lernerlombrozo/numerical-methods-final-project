f38dfe13beef65f3743e04881bd21e907c9b9e87
David Lerner <lernerlombrozo@gmail.com>
2019-11-24 20:09:56 -0700

    added one more function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let x0 = 0; // initial x
-let xf = 1.25*Math.PI; // final x
+let x0 = 1; // initial x
+let xf = 3; // final x
 let n = 2; // for composite plots
 let degree1=2;
 let degree2=3;
@@ -7,11 +7,12 @@ const smallN = 100; // this h is to make plot continuous.
 const xT="x";
 let analyticalSolution;
 let tol = 0.0001;
-let maxLevel = 10;
+let maxLevel = 1000;
 // let yT= "f(x)=sqrt(1-x^2)";
 // var yT= "f(x)=sin(x)";
 // var yT= "f(x)=x^6 - x^2 sin(2x)";
-var yT= "f(x)=cos(2x)/exp(x)";
+// var yT= "f(x)=cos(2x)/exp(x)";
+var yT= "f(x)=100/x^2*sin(10/x)";
 let toEvaluate="";
 
 function toSolve(x){
@@ -21,7 +22,8 @@ function toSolve(x){
   // return Math.sqrt(1-x**2); // Circle centered in (0,0) with radius 1. Can be evaluated from -1 to 1;
   // return Math.sin(x);
   // return x**6 - x**2 * Math.sin(2*x);
-  return Math.cos(2*x)/Math.exp(x);
+  // return Math.cos(2*x)/Math.exp(x);
+  return 100/x**2 * Math.sin(10/x)
 }
 
 var app = new Vue({
